c8d56c5f816fa2d0efd2a085060199e3f615cb69
Andres B <andres.b.dev@gmail.com>
2015-04-25 16:51:22 -0300

    Second test pass, clean templates

--- lib/hrq/hrq.rb
+++ lib/hrq/hrq.rb
@@ -2,5 +2,9 @@ module Hrq
   def add_template(file)
   end
 
+  def clean_templates
+  end
+
   module_function :add_template
+  module_function :clean_templates
 end
--- spec/lib/basic_tests_spec.rb
+++ spec/lib/basic_tests_spec.rb
@@ -5,5 +5,17 @@ RSpec.describe 'Basic usages' do
     it 'should allow you to add configuration files' do
       Hrq.add_template File.join(Dir.pwd, '..', 'templates', 'basic.rb')
     end
+
+    it 'should allows to clean all the templates configurations' do
+      Hrq.clean_templates
+    end
+
+    context 'using basic configuration' do
+      before do
+        Hrq.add_template File.join(Dir.pwd, '..', 'templates', 'basic.rb')
+      end
+
+      it 'should allows to translate a basic query'
+    end
   end
 end
